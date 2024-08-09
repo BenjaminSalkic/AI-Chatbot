91185c1124d8fd6e86a3a3aefb4604618891ca9a
Benjamin Salkic <89987994+BenjaminSalkic@users.noreply.github.com>
2024-08-09 02:02:27 +0200

    feat(chat): added chat functionalty

---
+++ frontend/src/components/chat/ChatItem.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Box, Avatar, Typography } from '@mui/material'
+import { useAuth } from '../../context/Authcontex'
+
+const ChatItem = ({content, role}:{content:string,role:'user'|'assistant'}) => {
+    const auth = useAuth();
+    return role ==='assistant' ? 
+    <Box sx={{display:'flex', p:2, bgcolor:'#004d5612', my:2, gap:2}}>
+        <Avatar sx={{ml:'0'}}>
+            <img src="" alt="openai" width={'30px'} />
+        </Avatar>
+        <Box><Typography fontSize={'20px'}>{content}</Typography></Box>
+    </Box> :
+    <Box sx={{display:'flex', p:2, bgcolor:'#004d56', my:2, gap:2}}>
+        <Avatar sx={{ml:'0',bgcolor:'black', color:'white'}}>
+            {auth?.user?.name[0]}
+            {auth?.user?.name.split(" ")[1][0]}
+        </Avatar>
+        <Box><Typography fontSize={'20px'}>{content}</Typography></Box>
+    </Box>
+}
+
+export default ChatItem
